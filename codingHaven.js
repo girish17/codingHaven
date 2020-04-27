22a656d6656797eb61c9c38790dcc5883d6e87ef
Girish M <girishm@mailfence.com>
2020-04-27 22:24:50 +0530

    1. added cancel button for answer 2. added clear button for question

--- codingHaven.js
+++ codingHaven.js
@@ -1,9 +1,15 @@
 /* @author: girish, elijah*/
 var count = 0
 document.getElementById("postQuestion").onclick =  function postQuestion() {
-    var question = '<span class="questionText">' + ++count + '. ' + document.getElementById("question").value + '</span><br>'
-    var button = '<input type="button" value="Answer" class="answerButton" id="answerButton" onclick="loadAnswerTextArea()">'
-    document.getElementById("postedQuestions").innerHTML += question+button
+    var question = document.getElementById("question").value
+    if(question.length != 0) {
+        var question = '<span class="questionText" id="question' + ++count + '">' + count + '. ' + document.getElementById("question").value + '</span><br>'
+        var button = '<input type="button" value="Answer" class="answerButton" id="answerButton" onclick="loadAnswerTextArea()">'
+        document.getElementById("postedQuestions").innerHTML += question+button
+    }
+    else {
+        alert("Please type a question and click on post")
+    }
 }
 
 function loadAnswerTextArea() {
@@ -12,7 +18,9 @@ function loadAnswerTextArea() {
     answerDiv.setAttribute("id", "answerDiv")
     answerDiv.innerHTML = '<textarea name="answerText" id="answerText" rows="20" cols="70" required="required" placeholder="Please type your answer here"></textarea>'
     var postAnswerButton = document.createElement('div')
-    postAnswerButton.innerHTML = '<input id="postAnswer" type="submit" value="Post Answer" onclick="postAnswer()">'
+    var inputBtn = '<input id="postAnswer" type="submit" value="Post Answer" onclick="postAnswer()">'
+    inputBtn += '<input id="cancelBtn" type="button" value="Cancel" onclick="cancel()">'
+    postAnswerButton.innerHTML = inputBtn
     document.getElementById("postedQuestions").appendChild(answerDiv)
     document.getElementById("answerText").after(postAnswerButton)  
 }
@@ -22,6 +30,18 @@ function postAnswer() {
     //Hide the answer div containg textarea and post question button
     document.getElementById("answerDiv").remove()
     document.getElementById("postedQuestions").innerHTML += answer
+}
+
+function cancel() {
+    document.getElementById("cancelBtn").remove()
+    document.getElementById("answerDiv").remove()
+    var button = document.createElement('input')
+    button.type = "button"
+    button.value = "Answer"
+    button.id = "answerButton"
+    button.onclick = loadAnswerTextArea
+    document.getElementById("question"+count).after(button)
+
 }
 
 document.getElementById("searchText").onkeydown = () => {
@@ -42,4 +62,8 @@ document.getElementById("searchText").onkeydown = () => {
         }
     }
 
+}
+
+document.getElementById("clearBtn").onclick = () => {
+    document.getElementById("question").value = ""
 }
